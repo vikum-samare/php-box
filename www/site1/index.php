af65f04921f910dc2ed8dcf5a182fea0d8a61074
vikum-samare <vikum@vikum.xyz>
2019-10-23 22:32:10 +0530

    Added 2 site folders and updated docker-compose file

---
+++ www/site1/index.php
@@ -0,0 +1,13 @@
+<!-- ./php/index.php -->
+
+<html>
+    <head>
+        <title>Hello World</title>
+    </head>
+
+    <body>
+        <?php
+            echo "Hello, World! site under maintenance";
+        ?>
+    </body>
+</html>
